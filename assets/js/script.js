2a5ba7e9df97d827c701765b863256a730bc87a4
Isaque Colem <65116789+isaquecolem@users.noreply.github.com>
2022-09-30 14:58:53 -0300

    adding another pipe
    adding a new pipe with another height and some background color to the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const pipe2 = document.querySelector('.pipe2');
 const cloud1 = document.querySelector('.cloud1');
 const cloud2 = document.querySelector('.cloud2');
 const cloud3 = document.querySelector('.cloud3');
@@ -15,15 +16,19 @@ const jump = () => {
 const loop = setInterval(() => {
     
     const pipePosition = pipe.offsetLeft;
+    const pipe2Position = pipe2.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
     const cloud1Position = cloud1.offsetLeft;
     const cloud2Position = cloud2.offsetLeft;
     const cloud3Position = cloud3.offsetLeft;
 
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80 || pipe2Position <= 120 && pipe2Position > 0 && marioPosition < 80) {
 
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
+
+        pipe2.style.animation = 'none';
+        pipe2.style.left = `${pipe2Position}px`;
 
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
